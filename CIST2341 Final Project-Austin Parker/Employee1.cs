d420812a7d959d2a078fd2931780446e393d03d3
Local User <Local User@DESKTOP-N1NMMQ0>
2020-07-19 16:35:40 -0400

    Add project files.

---
+++ CIST2341 Final Project-Austin Parker/Employee1.cs
@@ -0,0 +1,130 @@
+﻿/* Programmer: Austin Parker
+   Date: March 29th, 2020
+   Purpose: This program allows user to see breakdown of employees pay
+ */
+
+using System;
+using static System.Console;
+
+namespace CIST2341_Final_Project_Austin_Parker
+{
+    class Employee
+    {
+        string name;
+        double basePay;
+        double sales;
+
+        //Constructor
+        public Employee(string empName, double empBasePay, double empSales)
+        {
+            Name = empName;
+            BasePay = empBasePay;
+            Sales = empSales;
+        }
+
+        //Properties
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        public double BasePay
+        {
+            get
+            {
+                return basePay;
+            }
+            set
+            {
+                basePay = value;
+            }
+        }
+        public double Sales
+        {
+            get
+            {
+                return sales;
+            }
+            set
+            {
+                sales = value;
+            }
+        }
+
+        //Methods for calculating values
+        public double CalculateCommission()
+        {
+            double baseSalary, netSales;
+            double commission;
+            double remainSales;
+            baseSalary = basePay / 12;
+            netSales = sales - (10 * baseSalary);
+            if (netSales <= 0)
+                commission = 0;
+            else
+            {
+                if (netSales < 10000)
+                {
+                    commission = 0.05 * netSales;
+                }
+                else
+                {
+                    commission = 500;
+                    remainSales = netSales - 10000;
+                    if (remainSales < 15000)
+                    {
+                        commission += 0.1 * remainSales;
+                    }
+                    else
+                    {
+                        commission += 1500;
+                        remainSales -= 15000;
+                        if (remainSales < 25000)
+                        {
+                            commission += 0.15 * remainSales;
+                        }
+                        else
+                        {
+                            commission += 3750;
+                            remainSales -= 25000;
+                            commission += 0.2 * remainSales;
+                        }
+                    }
+                }
+            }
+            return commission;
+        }
+
+        //Calculates total pay
+        public double CalculateGross(double commission)
+        {
+            double grossPay;
+            grossPay = (basePay / 12) + commission;
+            return grossPay;
+        }
+
+        //Displays final information
+        public void PrintEmployee()
+        {
+
+            Write("Below is " + name + "'s" + " breakdown of pay: ");
+            double baseSalary = basePay / 12;
+            Write("\n\nBase Salary = $" + string.Format("{0:n}", baseSalary) + " ");
+            Write("Sales = $" + string.Format("{0:n}", sales) + " ");
+            double com = CalculateCommission();
+            Write("Conmmission = $" + string.Format("{0:n}", CalculateCommission()) + " ");
+            WriteLine("Gross Pay = $" + string.Format("{0:n}", CalculateGross(com)) + " ");
+            WriteLine();
+
+        }
+    }
+
+
+}
+
---
+++ CIST2341 Final Project-Austin Parker/Program.cs
@@ -0,0 +1,86 @@
+﻿/* Programmer: Austin Parker
+   Date: March 29th, 2020
+   Purpose: This program allows user to see breakdown of employees pay
+ */
+
+using System;
+using static System.Console;
+
+
+namespace CIST2341_Final_Project_Austin_Parker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            double annualSalary;
+            double monthlySales;
+
+            Employee[] emp = new Employee[20];
+            int i = 0;
+            do
+            {
+                //Welcome screen
+                Write("Thanks for using the Employee Gross Pay Calculator!\n" +
+                    "\nEnter Employee's name OR \"Q\" to end program : ");
+                name = ReadLine();
+                WriteLine();
+
+                //Requires employee name or "Q" to continue program
+                while (string.IsNullOrEmpty(name))
+                {
+                    WriteLine("Please enter employee's name or \"Q\" to end program");
+                    name = ReadLine();
+                }
+
+                //Closes program if "Q" is entered rather than employee name
+                if (name == "q" || name == "Q")
+                {
+                    WriteLine("Have a nice day, Goodbye!");
+                    Read();
+                    break;
+
+                }
+
+                do
+                {
+                    Write("Enter Annual Salary (Enter \"0\" for commission only employees ):\n");
+                    annualSalary = double.Parse(ReadLine());
+                    if (annualSalary == 0)
+                        break;
+                    //Continues to ask for annual salary if invalid input is entered. Will not let user continue
+                    while (annualSalary > 120000 || annualSalary < 12000)
+                    {
+                        WriteLine("\nIncorrect salary value, please input salary less than $120000.\n");
+                        WriteLine("Re-enter Salary: ");
+                        annualSalary = double.Parse(ReadLine());
+
+                    }
+                } while (annualSalary > 120000 || annualSalary < 12000);
+                WriteLine();
+
+                do
+                {
+                    Write("Enter Monthly Sales:");
+                    monthlySales = double.Parse(ReadLine());
+                    Clear();
+                    //Continues to ask for monthly sales if invalid input is entered. Will not let user continue
+                    while (monthlySales < 0)
+                    {
+                        WriteLine("\nIncorrect value, please input monthly sales of \"0\" or more.\n");
+                        WriteLine("Re-enter Sales");
+                        monthlySales = double.Parse(ReadLine());
+
+                    }
+                } while (monthlySales < 0);
+                emp[i] = new Employee(name, annualSalary, monthlySales);
+                emp[i].PrintEmployee();
+                i++;
+            } while (name != "q" || name != "Q");
+
+
+
+        }
+    }
+}
